7b589e29b94b83e47ca49ecbba38fe86044ad185
Sisir8642 <baburambista01@gmail.com>
2026-04-21 02:40:02 +0545

    fix:fix the migrations apply as we are providing the custom user; along with the model defined for tenants & user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from tenants.models import Tenant
 
-# Create your models here.
+class User(AbstractUser):
+    
+    ROLE_CHOICES = (
+        ('superadmin', 'Super Admin'),
+        ('tenantadmin', 'Tenant Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
