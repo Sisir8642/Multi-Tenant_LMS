1dd6f0503a1840e22ad477ab8dec529ba3f08726
Sisir8642 <baburambista01@gmail.com>
2026-04-21 15:57:51 +0545

    feat:intial setup for react(frontend), setup the auth context/provider and baseAPI setup

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  Dashboard = () => {
+  return (
+    <div> simple Dashboard page</div>
+  )
+}
+
+export default  Dashboard
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div> simple Login page</div>
+  )
+}
+
+export default Login
